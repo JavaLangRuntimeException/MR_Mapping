034fb3c233e374d6574bc08d4ee85a37f5c539e9
tarakokko3233 <tanahashist1230@outlook.jp>
2024-04-16 12:05:17 +0900

    トークン設定

--- MR_Mapping_API/urls.py
+++ MR_Mapping_API/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from MR_Mapping_API_app.views import RoomViewSet, NaviViewSet, UserViewSet
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,4 +10,5 @@ router.register(r'navis', NaviViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
