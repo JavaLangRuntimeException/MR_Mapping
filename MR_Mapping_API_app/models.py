5ea7c5eca3626a5646a9b068e5bda756ce9e16f2
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-04-16 11:45:51 +0900

    Update models.py

--- MR_Mapping_API_app/models.py
+++ MR_Mapping_API_app/models.py
@@ -5,7 +5,7 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms', default=1)
+    
 
     def __str__(self):
         return self.name
@@ -13,6 +13,7 @@ class Room(models.Model):
 class Navi(models.Model):
     dep_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='navi_dep')
     arr_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='navi_arr')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms', default=1)
 
     def __str__(self):
         return self.name
