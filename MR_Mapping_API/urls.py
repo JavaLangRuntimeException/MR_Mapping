b2a4bd63dd73f175762efc3aca66c1ae09708535
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-04-16 18:58:22 +0900

    Update urls.py

--- MR_Mapping_API/urls.py
+++ MR_Mapping_API/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from MR_Mapping_API_app.views import RoomViewSet, NaviViewSet, UserViewSet, LoginViewSet
 from rest_framework.authtoken import views
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
