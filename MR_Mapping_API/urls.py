886389e128070ea5cf68a2291eb2cff8b37b1d24
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-04-19 17:44:02 +0900

    Update urls.py

--- MR_Mapping_API/urls.py
+++ MR_Mapping_API/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from MR_Mapping_API_app.views import RoomViewSet, NaviViewSet, UserViewSet, LoginViewSet
+from MR_Mapping_API_app.views import RoomViewSet, NaviViewSet, UserViewSet, LoginViewSet, NaviCreateView
 from rest_framework.authtoken import views
 from django.contrib import admin
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
+    path('navi/create/', NaviCreateView.as_view(), name='navi-create'),
 ]
