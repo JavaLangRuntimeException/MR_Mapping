d65f8956b3d0e00f63b76ea620129e538402ed1b
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-06-20 14:27:14 +0900

    Update room.go

--- models/room.go
+++ models/room.go
@@ -1,9 +1,11 @@
 package models
 
 // User はユーザーを表すデータ構造体
+package models
+
 type Room struct {
-	ID   int    `json:"id" db:"id"`     // ユーザーのID
-	Name string `json:"name" db:"name"` // ユーザーの名前
-	X    string `json:"x" db:"x"`
-	Y    string `json:"y" db:"y"`
+    ID   int    `json:"id"`
+    Name string `json:"name"`
+    X    int    `json:"x"`
+    Y    int    `json:"y"`
 }
