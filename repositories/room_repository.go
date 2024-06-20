f80afa18feea7c376196a406573965626e585e58
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-06-20 14:26:52 +0900

    Update room_repository.go

--- repositories/room_repository.go
+++ repositories/room_repository.go
@@ -8,23 +8,23 @@ import (
 
 // すべてのユーザーをデータベースから取得
 func GetAllRooms() ([]models.Room, error) {
-	var rooms []models.Room
-	query := "SELECT id, name, x, y FROM rooms"
-	rows, err := database.DB.Query(context.Background(), query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+    var rooms []models.Room
+    query := "SELECT id, name, x, y FROM rooms"
+    rows, err := database.DB.Query(context.Background(), query)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
 
-	for rows.Next() {
-		var room models.Room
-		if err := rows.Scan(&room.ID, &room.Name, &room.X, &room.Y); err != nil {
-			return nil, err
-		}
-		rooms = append(rooms, room)
-	}
+    for rows.Next() {
+        var room models.Room
+        if err := rows.Scan(&room.ID, &room.Name, &room.X, &room.Y); err != nil {
+            return nil, err
+        }
+        rooms = append(rooms, room)
+    }
 
-	return rooms, nil
+    return rooms, nil
 }
 
 // 指定されたIDのユーザーをデータベースから取得
